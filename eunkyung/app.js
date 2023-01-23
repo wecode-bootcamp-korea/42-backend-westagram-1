353ef8c335c1532cc85e861ae44c5f3f9a62d793
eunkyung <dmsrud9294@gmail.com>
2023-01-23 18:21:39 +0900

    const dotenv 삭제

--- eunkyung/app.js
+++ eunkyung/app.js
@@ -2,7 +2,6 @@ require("dotenv").config()
 
 const express = require('express')
 const cors = require('cors')
-const dotenv = require('dotenv')
 const morgan = require('morgan')
 const { DataSource } = require('typeorm')
 
